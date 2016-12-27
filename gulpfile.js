35b4b7e0a91717ac156322d2b5dab95eba59fc21
Aaron <apurjh@gmail.com>
2016-12-27 21:17:06 +0000

    gulp turns it to minified ie8 css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 const livereload = require('gulp-livereload');
+const cleanCSS = require('gulp-clean-css');
+const flatten = require('gulp-flatten');
+const plumber = require('gulp-plumber');
 
 const src  = 'src';
 const dist = 'public';
@@ -8,7 +11,10 @@ const dist = 'public';
 gulp.task('sass', () => {
   return gulp.src(`${src}/**/*.scss`)
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest(`${dist}`))
+      .pipe(cleanCSS({ compatibility: 'ie8' }))
+      .pipe(plumber())
+      .pipe(flatten())
+      .pipe(gulp.dest(`${dist}/css/`))
       .pipe(livereload());
 });
 
