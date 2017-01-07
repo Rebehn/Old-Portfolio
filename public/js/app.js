b6dc2c7c32d54a648136ca02e1a335e94e5ca6aa
Aaron <apurjh@gmail.com>
2017-01-07 18:37:24 +0000

    way more styling

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,1 @@
-"use strict";$(document).ready(function(){$("#particles").particleground({dotColor:"white",lineColor:"white",lineWidth:.5,proximity:100,maxSpeedX:.2,maxSpeedY:.2})});
+"use strict";$(document).ready(function(){$("#particles").particleground({dotColor:"#00471B",lineColor:"#00471B",lineWidth:1,particleRadius:5,proximity:90,maxSpeedX:.2,maxSpeedY:.2,parallaxMultiplier:8})});
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $('#particles').particleground({
-    dotColor: 'white',
-    lineColor: 'white',
-    lineWidth: 0.5,
-    proximity: 100,
+    dotColor: '#00471B',
+    lineColor: '#00471B',
+    lineWidth: 1,
+    particleRadius: 5,
+    proximity: 90,
     maxSpeedX: 0.2,
-    maxSpeedY: 0.2
+    maxSpeedY: 0.2,
+    parallaxMultiplier: 8
   });
 });
