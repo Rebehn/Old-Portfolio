2f155bea263d97a23960ab38baaaf3e7efb038f2
Aaron <apurjh@gmail.com>
2017-01-24 22:33:19 +0000

    added info for itrack

--- public/js/app-debug.js
+++ public/js/app-debug.js
@@ -20,7 +20,7 @@ $(function () {
   var $projectCardFour = $('#projectCardFour');
 
   $projectCardOne.on('click', showProjectOne);
-  $projectCardTwo.on('click', showProject);
+  $projectCardTwo.on('click', showProjectTwo);
   $projectCardThree.on('click', showProject);
   $projectCardFour.on('click', showProject);
 
@@ -28,6 +28,14 @@ $(function () {
     $shadeBox.show();
     $projectInfoBox.show();
     $projectInfoBox.html('<a href="#portfolio" id="closeProjectButtonLink"><div class="closeProjectButton">X</div></a><h3>Talkr</h3><p>My final project during the course, I decided to create a chatroom based instant messenger. Users can have private and public with each other or even just with themselves. With a Ruby-on-Rails api and an angular based front end it was my most complex project so far with Action Cable handling the instant part of Instant messenger.</p><a href="#"><div class="projectButton">View Code</div></a><a href="#"><div class="projectButton" id="viewCodeButton">View Project</div></a>');
+    var $closeProjectButton = $('.closeProjectButton');
+    $closeProjectButton.on('click', hideProject);
+  }
+
+  function showProjectTwo() {
+    $shadeBox.show();
+    $projectInfoBox.show();
+    $projectInfoBox.html('<a href="#portfolio" id="closeProjectButtonLink"><div class="closeProjectButton">X</div></a><h3>iTrack</h3><p>A budgeting app using data pulled from the USDA nutrition API. Users can set their own personal daily calorie goal and can log their meals each day to create charts that return information about what they have eaten over time. They can also track their calories, protein, fat, etc over the course of a day week or month.</p><a href="#"><div class="projectButton">View Code</div></a><a href="#"><div class="projectButton" id="viewCodeButton">View Project</div></a>');
     var $closeProjectButton = $('.closeProjectButton');
     $closeProjectButton.on('click', hideProject);
   }
